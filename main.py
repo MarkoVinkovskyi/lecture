bf40ffe393af04edf1e838318b80289c91592a19
MarkoVinkovskyi <marko.vinkovskyi@mipolytech.education>
2023-03-18 10:14:15 +0200

    Lesson 0.5

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-l = int(input("Введіть число: "))
-factorial = 1
-for i  in range(1,l+1):
-    factorial *=i
-print("Факторіад числа {} дорівнює {}".format(l,factorial))
+print("Для виходу натисніть Y")
+while True:
+    data=input("Введіть суму для обміну:")
+    if data.lower() == "y":
+        break
+    money = int(data)
+    if money <0:
+        print("Сума має бути позитивною")
+        continue
+    cache = round(money/56.2)
+    print("Да видачі {} доларів".format(cache))
+
+print("Робота обмінного пункту завершано")
