98b64132a461c7f00f1a92fe46b567b66e779c8f
MarkoVinkovskyi <marko.vinkovskyi@mipolytech.education>
2023-03-18 10:17:45 +0200

    Lesson 0.51

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 print("Для виходу натисніть Y")
 while True:
-    data=input("Введіть суму для обміну:")
+    data = input("Введіть суму для обміну:")
     if data.lower() == "y":
         break
     money = int(data)
-    if money <0:
+    if money < 0:
         print("Сума має бути позитивною")
         continue
     cache = round(money/56.2)
     print("Да видачі {} доларів".format(cache))
-
 print("Робота обмінного пункту завершано")
